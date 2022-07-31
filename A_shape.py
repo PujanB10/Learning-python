a61cfe60b8d337e5a10fb14c060c90df0c1fd34c
Pujan <68160782+PujanB10@users.noreply.github.com>
2022-07-31 17:24:13 +0545

    Create A_shape.py

---
+++ A_shape.py
@@ -0,0 +1,18 @@
+def triangle(num,char):
+    print(num*char)
+
+def rectangle(num,char):
+    print(num*char)
+
+def shape(num1,char1,num2,char2):
+    i,j=0,0
+    while i<=num1:
+        triangle(i,char1)
+        i+=1
+    while j<num2:
+        rectangle(num1,char2)
+        j+=1
+
+
+if __name__ == "__main__":
+    shape(5, "x", 2, "o")
